37ea04baba59e94de72e5c81c1efb9b259a55492
Rico Moorman <rico.moorman@gmail.com>
2024-08-17 03:48:58 +0200

    Add more nesting for reference

--- hello/lib/hello_web/live/form_live.ex
+++ hello/lib/hello_web/live/form_live.ex
@@ -5,7 +5,7 @@ defmodule HelloWeb.FormLive do
     use Ecto.Schema
     import Ecto.Changeset
 
-    defmodule NestedItem do
+    defmodule Nested do
       use Ecto.Schema
       import Ecto.Changeset
 
@@ -22,17 +22,37 @@ defmodule HelloWeb.FormLive do
       end
     end
 
+    defmodule NestedDeeper do
+      use Ecto.Schema
+      import Ecto.Changeset
+
+      @primary_key false
+      embedded_schema do
+        embeds_many :nested, Nested, on_replace: :delete
+      end
+
+      def changeset(data, attrs) do
+        data
+        |> cast(attrs, [])
+        |> cast_embed(:nested)
+      end
+    end
+
     @primary_key false
     embedded_schema do
       field :name, :string
 
-      embeds_many :nested, NestedItem, on_replace: :delete
+      embeds_many :nested, Nested, on_replace: :delete
+      embeds_many :nested_with_let, Nested, on_replace: :delete
+      embeds_many :nested_deeper, NestedDeeper, on_replace: :delete
     end
 
     def changeset(data, attrs) do
       data
       |> cast(attrs, [:name])
       |> cast_embed(:nested)
+      |> cast_embed(:nested_with_let)
+      |> cast_embed(:nested_deeper)
       |> validate_required(:name)
       |> validate_format(:name, ~r|a|, message: "must include the letter a")
     end
@@ -40,7 +60,13 @@ defmodule HelloWeb.FormLive do
 
   def handle_params(_, _, socket) do
     form =
-      %Schema{nested: [%Schema.NestedItem{name: "foo"}]}
+      %Schema{
+        nested: [%Schema.Nested{name: "foo"}],
+        nested_with_let: [%Schema.Nested{name: "foo"}],
+        nested_deeper: [
+          %Schema.NestedDeeper{nested: [%Schema.Nested{name: "foo"}]}
+        ]
+      }
       |> Schema.changeset(%{})
       |> to_form()
 
@@ -60,8 +86,22 @@ defmodule HelloWeb.FormLive do
       <.input field={@form[:name]} label="Name" />
 
       <div class="mt-10 p-2 border shadow">
-        <.inputs_for :let={nested} field={f[:nested]}>
+        <.inputs_for :let={nested} field={@form[:nested]}>
           <.input field={nested[:name]} label="Name" />
+        </.inputs_for>
+      </div>
+
+      <div class="mt-10 p-2 border shadow">
+        <.inputs_for :let={nested} field={f[:nested_with_let]}>
+          <.input field={nested[:name]} label="Name" />
+        </.inputs_for>
+      </div>
+
+      <div class="mt-10 p-2 border shadow">
+        <.inputs_for :let={nested_deeper} field={@form[:nested_deeper]}>
+          <.inputs_for :let={nested} field={nested_deeper[:nested]}>
+            <.input field={nested[:name]} label="Name" />
+          </.inputs_for>
         </.inputs_for>
       </div>
     </.form>
